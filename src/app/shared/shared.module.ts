d7dc712dc1e8669d499f3284e465e747e65b44d7
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-07-29 06:21:59 +0300

    up

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { AlertComponent } from './alert/alert.component';
 import { EventBlockerDirective } from './directives/event-blocker.directive';
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, AlertComponent, EventBlockerDirective],
+  declarations: [
+    LoaderComponent,
+    AlertComponent,
+    EventBlockerDirective,
+    SlicePipe,
+  ],
   imports: [CommonModule],
-  exports: [LoaderComponent, AlertComponent, EventBlockerDirective],
+  exports: [LoaderComponent, AlertComponent, EventBlockerDirective, SlicePipe],
 })
 export class SharedModule {}
