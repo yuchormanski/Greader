86f51e3f1f79d277e9ff70bdb2270174e2020613
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-08-08 07:28:13 +0300

    up

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Book } from './types/book';
-import { environment } from 'src/environments/environment';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Book } from './types/book';
+// import { environment } from 'src/environments/environment';
 
-// const { apiUrl } = environment;
+// // const { apiUrl } = environment;
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ApiService {
-  constructor(private http: HttpClient) {}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ApiService {
+//   constructor(private http: HttpClient) {}
 
-  getBooks() {
-    return this.http.get<Book[]>(`/api/books`);
-  }
+//   getBooks() {
+//     return this.http.get<Book[]>(`/api/books`);
+//   }
 
-  getOneBook(bookId: string) {
-    return this.http.get<Book>(`/api/books/${bookId}`);
-  }
+//   getOneBook(bookId: string) {
+//     return this.http.get<Book>(`/api/books/${bookId}`);
+//   }
 
-  getFooterBooks() {
-    return this.http.get<Book[]>(`/api/books?sortBy=\_createdOn%20`);
-  }
-}
+//   getFooterBooks() {
+//     return this.http.get<Book[]>(`/api/books?sortBy=\_createdOn%20`);
+//   }
+// }
